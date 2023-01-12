9fc8f4829fdf3f88f5d03272c1316a1f95bf720a
sske0 <akbaraz@code.edu.az>
2023-01-13 01:37:33 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// 1
+//int sum = 0;
+//int reminder = 0;
+//Console.WriteLine("enter ur number: ");
+//int number = Convert.ToInt32(Console.ReadLine());
+//while(number > 0)
+//{
+//    reminder = number % 10;
+//    sum += reminder;
+//    number /= 10;
+//}
+//if (sum % 2 == 0)
+//{
+//    Console.WriteLine(value: $"{sum} is even");
+//}
+//else
+//{
+//    Console.WriteLine(value: $"{sum} is odd");
+
+//}
+
+//2
+//int sum = 0;
+//for (int i = 100; i > 0; i--)
+//{
+//    if (i % 13 == 0)
+//    {
+//        sum += i;
+//        Console.WriteLine(i);
+//        continue;
+//    }
+//    else
+//    {
+//        continue;
+//    }
+//}
+//Console.WriteLine(sum);
+
+//3
+//int reminder = 0;
+//int min_digit = 0;
+//Console.WriteLine("enter ur number: ");
+//int number = Convert.ToInt32(Console.ReadLine());
+
+//while (number > 0)
+//{
+//    reminder = number % 10;
+//    number = number / 10;
+//    min_digit = reminder;
+
+//    if (min_digit < reminder)
+//    {
+//        min_digit = reminder;
+//        continue;
+//    }
+//    else if (min_digit == 0) break;
+//    else continue;
+    
+//}
+//Console.WriteLine(min_digit);
+
